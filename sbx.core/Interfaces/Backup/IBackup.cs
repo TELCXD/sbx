87f1189917999e801c220271dae8244942aca3f7
telcxd <rubencordobacuellar@gmail.com>
2025-06-07 12:05:34 -0500

    Se agrega logica para copia de seguridad

---
+++ sbx.core/Interfaces/Backup/IBackup.cs
@@ -0,0 +1,9 @@
+﻿using sbx.core.Entities;
+
+namespace sbx.core.Interfaces.Backup
+{
+    public interface IBackup
+    {
+        Task<Response<dynamic>> Create(string Ruta_backup, string NombreCopiaSeguridad);
+    }
+}
